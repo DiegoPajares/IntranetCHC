8463c30f436792d6cd6e5074f5ccaadab4dde58c
eljarena <chavez.3005@hotmail.com>
2017-11-23 18:55:04 -0500

    miraaaaaaaaaaaaaaaaaaaa carta fianza

--- application/controllers/CartaFianza.php
+++ application/controllers/CartaFianza.php
@@ -1,8 +1,58 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+defined('BASEPATH') OR exit('No direct script access allowed');
 
+class CartaFianza extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+        if (!$this->session->userdata('login')) {
+            header("Location: " . MAIN_URL);
+        }
+        $this->load->model('CartaFianza_model');
+    }
+
+    public function index() {
+        $data['actualP'] = 'CartaFianza';
+        $data['actualH'] = 'CartaFianza';
+        $data['main_content'] = 'cartafianza_view';
+        $data['page_assets'] = 'advance_form';
+        $data['titulo'] = 'INTRANET | CartaFianza';
+        $this->load->view('master/template', $data);
+    }
+
+    public function CartaFianza_lista() {
+        $data = json_encode($this->CartaFianza_model->cartafianzaQry_listar());
+        return print_r($data);
+    }
+
+    public function CartaFianzaDet_listaxIDCF() {
+        $data = json_encode($this->CartaFianza_model->cartafianzadetQry_getxidCF());
+        return print_r($data);
+    }
+
+    public function CartaFianza_listaxID() {
+        $data = json_encode($this->CartaFianza_model->cartafianzaQry_getxid());
+        return print_r($data);
+    }
+    
+    public function CartaFianza_actualizaEstado() {
+        $data = $this->CartaFianza_model->cartafianzaQry_updestado();
+        return print_r($data);
+    }
+    
+    public function CartaFianzaFec_actualizaEstado() {
+        $data = $this->CartaFianza_model->cartafianzafecQry_ins();
+        return print_r($data);
+    }
+
+    
+    public function CartaFianza_registrar() {
+        if (!empty($_POST["txtIdEditar"])) {
+            $data = $this->CartaFianza_model->cartafianzaQry_upd();
+        } else {
+            $data = $this->CartaFianza_model->cartafianzaQry_ins();
+        }
+        return print_r($data);
+    }
+}
--- application/models/CartaFianza_model.php
+++ application/models/CartaFianza_model.php
@@ -1,8 +1,185 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+class CartaFianza_model extends CI_Model {
 
+    function cartafianzaQry_listar() {
+
+        $this->db->select('*');
+        $this->db->from('cartafianza');
+        $this->db->where('estado', 1);
+        $this->db->or_where('estado', 0);
+        $this->db->or_where('estado', 2);
+        $this->db->order_by('id', 'DESC');
+        $query = $this->db->get();
+        
+        if (count($query) > 0) {
+            return $query->result();
+        } else {
+            return null;
+        }
+    }
+
+    function cartafianzaQry_getxid() {
+        if (isset($_POST['id'])) {
+            $idcartafianza = $_POST['id'];
+        }
+        $query = $this->db->query('SELECT * FROM cartafianza WHERE Id= "' . $idcartafianza . '";');
+        if (count($query) > 0) {
+            return $query->result();
+        } else {
+            return null;
+        }
+    }
+    
+    function cartafianzadetQry_getxidCF() {
+        if (isset($_POST['id'])) {
+            $idcartafianza = $_POST['id'];
+        }
+         $this->db->select('d.*,dd.*');
+        $this->db->from('cartafianza d');
+        $this->db->join('cf_fechas dd', 'dd.det_cartafianza_id = d.id');
+        $this->db->where('d.CartaFianza_id', $idcartafianza);
+        $this->db->order_by('d.id', 'DESC');
+        $query = $this->db->get();
+        
+        if (count($query) > 0) {
+            return $query->result();
+        } else {
+            return null;
+        }
+    }
+    
+    
+    function CartaFianzaQry_updestado() {
+        if (isset($_REQUEST['id'])) {
+            $id = $_REQUEST['id'];
+        }
+        if (isset($_REQUEST['estado'])) {
+            $estado = $_REQUEST['estado'];
+        }
+
+        $this->db->set('estado', $estado, FALSE);
+        $this->db->where('id', $id);
+        $this->db->update('cartafianza');
+    }
+
+    function cartafianzaQry_ins() {
+        $FielCumplimiento = null;
+        $numero = null;
+        $gastofinac = null;
+        $montorenov = null;
+        $fechaemisionini = null;
+        $fechavencren = null;
+        $obras_id = null;
+        
+        
+        if (isset($_POST['txtFielCumplimiento'])) {
+            $FielCumplimiento = $_POST['txtFielCumplimiento'];
+        }
+        if (isset($_POST['txtnumero'])) {
+            $numero = $_POST['txtnumero'];
+        }
+        if (isset($_POST['txtgastofinac'])) {
+            $gastofinac = $_POST['txtgastofinac'];
+        }
+        if (isset($_POST['txtfechaemision'])) {
+            $fechaemisionini = trim($_POST['txtfechaemision']);
+            $fechaemisionini = DateTime::createFromFormat('d/m/Y', $fechaemisionini)->format('Y-m-d');
+        }
+        if (isset($_POST['txtfechaven'])) {
+            $fechavencren = trim($_POST['txtfechaven']);
+            $fechavencren = DateTime::createFromFormat('d/m/Y', $fechavencren)->format('Y-m-d');
+        }
+        if (isset($_POST['txtmontorenov'])) {
+            $montorenov = $_POST['txtmontorenov'];
+        }
+        if (isset($_POST['cboObras'])) {
+            $obras_id = $_POST['cboObras'];
+        }
+        
+        
+        
+        $data = array(
+            'obras_id' => $obras_id,
+            'FielCumplimiento' => $FielCumplimiento,
+            'numero' => $numero,
+            'gastofinac' => $gastofinac,
+            'montorenov' => $montorenov,
+            'fechaemisionini' => $fechaemisionini,
+            'fechavencren' => $fechavencren
+        );
+        $this->db->insert('cartafianza', $data);
+    }
+    
+    function cartafianzafecQry_ins() {
+        if (isset($_POST['cf_id'])) {
+            $cartafianza_id = $_POST['cf_id'];
+        }
+        if (isset($_POST['txtfechaemision'])) {
+            $fechaemision = trim($_POST['txtfechaemision']);
+            $fechaemision = DateTime::createFromFormat('d/m/Y', $fechaemision)->format('Y-m-d');
+        }
+        if (isset($_POST['txtfechavencimiento'])) {
+            $fechavencimiento = trim($_POST['txtfechavencimiento']);
+            $fechavencimiento = DateTime::createFromFormat('d/m/Y', $fechavencimiento)->format('Y-m-d');
+        }
+         $data = array(
+            'cartafianza_id' => $cartafianza_id,
+            'fechaemision' => $fechaemision,
+            'fechavencimiento' => $fechavencimiento,
+            'es_amortizado' => 1,
+        );
+        $this->db->insert('cf_fechas', $data);
+    }
+    function cartafianzaQry_upd() {
+
+        if (isset($_POST['txtIdEditar'])) {
+            $editarID = $_POST['txtIdEditar'];
+        }
+
+         $FielCumplimiento = null;
+        $numero = null;
+        $gastofinac = null;
+        $montorenov = null;
+        $fechaemisionini = null;
+        $fechavencren = null;
+        $obras_id = null;
+        
+        
+        if (isset($_POST['txtFielCumplimiento'])) {
+            $FielCumplimiento = $_POST['txtFielCumplimiento'];
+        }
+        if (isset($_POST['txtnumero'])) {
+            $numero = $_POST['txtnumero'];
+        }
+        if (isset($_POST['txtgastofinac'])) {
+            $gastofinac = $_POST['txtgastofinac'];
+        }
+        if (isset($_POST['txtfechaemision'])) {
+            $fechaemisionini = trim($_POST['txtfechaemision']);
+            $fechaemisionini = DateTime::createFromFormat('d/m/Y', $fechaemisionini)->format('Y-m-d');
+        }
+        if (isset($_POST['txtfechaven'])) {
+            $fechavencren = trim($_POST['txtfechaven']);
+            $fechavencren = DateTime::createFromFormat('d/m/Y', $fechavencren)->format('Y-m-d');
+        }
+        if (isset($_POST['txtmontorenov'])) {
+            $montorenov = $_POST['txtmontorenov'];
+        }
+        if (isset($_POST['cboObras'])) {
+            $obras_id = $_POST['cboObras'];
+        }
+        $data = array(
+            'obras_id' => $obras_id,
+            'FielCumplimiento' => $FielCumplimiento,
+            'numero' => $numero,
+            'gastofinac' => $gastofinac,
+            'montorenov' => $montorenov,
+            'fechaemisionini' => $fechaemisionini,
+            'fechavencren' => $fechavencren
+        );
+        $this->db->where('id', $editarID);
+        $this->db->update('cartafianza', $data);
+    }
+
+}
