4917f7a5a1ca037981214d5411a3fb8fe7e87fbc
Diegoupnorte <Diego@McKA0n9ny0Mus3.local>
2017-12-05 14:07:49 -0500

    DomPDF

---
+++ application/views/reportes/reporte_prueba.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+
+    </head>
+    <body>
+        <!--<h2><?php echo $titulo; ?></h2>-->        
+        <h4>Proceso:</h4>
+        <h4>Fecha de contrato:</h4>
+        <table border="1" cellspacing="3" cellpadding="4" style="width: 100%;">
+            <tr>
+                <th>#</th>
+                <th align="right">RIGHT align</th>
+                <th align="left">LEFT align</th>
+                <th>4A</th>
+            </tr>
+            <tr>
+                <td align="center">1</td>
+                <td align="center" colspan="2">23</td>
+                <td align="center">4</td>
+            </tr>            
+        </table>
+    </body>
+</html>
