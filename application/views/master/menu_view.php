4dd9958ef867a4a0c5e2ac953442063584f19e87
Diegoupnorte <Diego@McKA0n9ny0Mus3.local>
2017-12-05 20:09:48 -0500

    Menu Fix

--- application/views/master/menu_view.php
+++ application/views/master/menu_view.php
@@ -75,30 +75,7 @@
                                 <i class="fa fa-file-text-o" aria-hidden="true"></i>
                                 <span>Por Pagar</span>
                             </a>                        
-                        </li>
-                        <li class="nav-parent <?php echo (($actualP == 'Reportes') ? 'nav-expanded nav-active' : ''); ?>">
-                            <a class="nav-link" href="#">
-                                <i class="fa fa-file-pdf-o" aria-hidden="true"></i>
-                                <span>Reportes</span>
-                            </a>
-                            <ul class="nav nav-children">
-                                <li class="<?php echo (($actualH == 'Reportes/Amortizaciones') ? 'nav-active' : ''); ?>">
-                                    <a class="nav-link" href="<?php echo MAIN_URL ?>/Reportes/Amortizaciones">
-                                        Amortizaciones
-                                    </a>
-                                </li>
-                                <li class="<?php echo (($actualH == 'Reportes/Pagos') ? 'nav-active' : ''); ?>">
-                                    <a class="nav-link" href="<?php echo MAIN_URL ?>/Reportes/ControlPagos">
-                                        Control de Pagos
-                                    </a>
-                                </li>
-                                <li class="<?php echo (($actualH == 'Reportes/CartaFianza') ? 'nav-active' : ''); ?>">
-                                    <a class="nav-link" href="<?php echo MAIN_URL ?>/Reportes/CartaFianza">
-                                        Cartas Fianza
-                                    </a>
-                                </li>
-                            </ul>
-                        </li>
+                        </li>                        
                     </ul>
                 </nav>
 
